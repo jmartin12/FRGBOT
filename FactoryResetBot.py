a1748af2020aff3533ae9d8f0869569fc7e61f37
jmartin12 <jmartin12@gulls.salisbury.edu>
2017-06-22 16:30:57 -0400

    Add files via upload

--- FactoryResetBot.py
+++ FactoryResetBot.py
@@ -4,7 +4,7 @@ Created by KJ
 6/22/17
 Doing the LAWDS work
 """
-
+import random
 import discord
 import logging
 
@@ -32,9 +32,34 @@ server ID, as of now its my testBotServer
 serverID = 327436572175630349
 
 
+"""
+Client object to communicate to a server with a bot
+"""
 client = discord.Client()
 
+"""
+random seed generated only once
+"""
+random.seed()
 
+"""
+me :)
+"""
+theProphet = "Jacob"
+
+
+"""
+Array of nicknames in our server.
+"""
+nickNames = [ theProphet, "Carrigan", "RIOT MARTINJSTN1", "Jackson", "grand maester simpson", "Joe W", "Chris L", "Joseph" ]
+
+
+####
+#   Some notes:
+#   for a message object, when referring to the user who sent the message
+#   if private channel -> message.user.display_name (this is nickname for this object)
+#   if public channel -> message.author.nick
+####
 
 
 @client.event
@@ -46,14 +71,65 @@ async def on_message(message):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
 
-    if message.content.startswith('f!help'):
+    elif message.content.startswith('f!help'):
         msg = """ Welcome to the Factory Reset Games Bot! \n
-                Issue command with f! \n
-                List of available command: \n
+                Issue commands with f! \n
+                List of available commands: \n
                  f!help
                  f!hello
+                 f!penissize <name> (BRACKETS INCLUDED)
                 """.format(message)
         await client.send_message(message.channel, msg)
+
+    elif message.content.startswith('f!penissize'):
+        await detectParamater(message)
+        size = await getPenisSize(message)
+        msg = '%s penis size is about %d inches short' %(message.author.nick ,size)
+        await client.send_message(message.channel, msg)
+
+    else:
+        await client.send_message(message.channel, sys_error(1))
+
+#return a penis size int. 
+async def getPenisSize(m):
+    print(m.content)
+    if m.channel.is_private:
+        if m.user.display_name == theProphet:
+            return 1000000000
+        else:
+            return random.randint(1,8)
+    else:
+        if m.author.nick == theProphet:
+            return 100000000
+        else:
+            print(m.author.nick)
+            return random.randint(1,8)
+    
+
+#returns the first paramater with delimeters '<' '>'
+async def detectParamater(message):
+    found = False
+    spot = 0
+    for i in range(len(message.content)):
+        if message.content[i] == '<':
+            spot = i
+            found = True
+            break      #TODO, ERROR CHECK NO PARAMS.
+    if found:
+        originalParamaterName = await parseMessageParamaterName(i, message)
+    else:
+        return -1
+
+#this returns a string of paramter name passed in by < and > delimiters
+async def parseMessageParamaterName(index, message): #TODO, errorcheck no >
+    cont = True
+    name = ""
+    #TODO parse up until > then return
+
+
+async def sys_error(num):
+    if num == 1:
+        return "Invalid syntax! type f!help for more information"
 
 @client.event
 async def on_ready():
