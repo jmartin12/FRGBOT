6da967b302abea17a4c34720741ad79760022a5e
jmartin12 <jmartin12@gulls.salisbury.edu>
2017-06-23 15:54:59 -0400

    Add files via upload
    Added parameter recolonization

--- FactoryResetBot.py
+++ FactoryResetBot.py
@@ -53,6 +53,27 @@ Array of nicknames in our server.
 """
 nickNames = [ theProphet, "Carrigan", "RIOT MARTINJSTN1", "Jackson", "grand maester simpson", "Joe W", "Chris L", "Joseph" ]
 
+"""
+IMPORTANT.
+THIS FOLLOWS THE SAME INDEX AS ABOVE. SO INDEX 0 is JACOB, 1 is CARRIGAN.
+THE ID's need to correspond with the index.
+"""
+nickNameID = [246389497053446144,  143040399454240768]
+
+
+"""
+indecees for our arrays so we can refer to people by name
+"""
+Jacob=0
+Carrigan=1
+Justin=2
+Jackson=3
+John=4
+LilJoe=5
+Chris=6
+Joseph=7
+
+
 
 """
 Array of commands that take NO paramaters
@@ -67,13 +88,14 @@ advCommands = ["f!penissize"]
 ####
 #   Some notes:
 #   for a message object, when referring to the user who sent the message
-#   if private channel -> message.user.display_name (this is nickname for this object)
+#   if private channel -> message.user.display_name (this is nickname for the user who sent this object)
 #   if public channel -> message.author.nick
 ####
 
 
 @client.event
 async def on_message(message):
+    
     if message.author == client.user:
         return
 
@@ -92,7 +114,18 @@ async def on_message(message):
         await client.send_message(message.channel, msg)
 
     elif message.content.startswith('f!penissize'):
-        if await detectParamater(message):
+        paramType = await detectParamater(message)
+        if paramType==2:
+            tempName = await scanIDs(message)
+            tempName = tempName[:-1]
+            paramaterName = '<@' + tempName
+           # print(paramaterName)
+            #if await detectJacob(message):
+             #   size = 10000000000
+            #else:
+            size = await getPenisSize(message)
+            msg = '%s penis size is about %d inches short!' %(paramaterName + "'s", size)
+        elif paramType==1:
             paramaterName = await parseMessageParamaterName(message)
             size = await getPenisSize(message)
             msg = '%s penis size is about %d inches short!' %(paramaterName + "'s", size)
@@ -106,20 +139,13 @@ async def on_message(message):
 
 #return a penis size int. 
 async def getPenisSize(m):
-    if m.channel.is_private:
-        if m.user.display_name == theProphet:
-            return 1000000000
-        else:
-            return random.randint(1,8)
-    else:
-        if m.author.nick == theProphet:
-            return 100000000
-        else:
-            print(m.author.nick)
-            return random.randint(1,8)
-    
-
-#returns the first paramater with delimeters '<' '>'
+    return random.randint(1,8)
+"""    
+returns an int depending if < or > is detected
+0 - No param found
+1 - Param found, basic text. i.e., <jacob>
+2 - Param found, user ID. i.e., <@Jacob>
+"""
 async def detectParamater(message):
     found = False
     found2 =False
@@ -131,12 +157,25 @@ async def detectParamater(message):
             break      #TODO, ERROR CHECK NO PARAMS.
 
     for spot in range(len(message.content)):
+        if message.content[spot] == '@':
+            idName=True
         if message.content[spot] == '>':
-            found2 = True;
-            break;
-        
-    if found and found2:
-        return True #meaning we have the proper format for params like <text>
+            found2 = True
+            break
+    
+    if found and found2 and idName:
+        return 2
+    elif found and found2:
+        return 1 
+    else:
+        return 0
+
+async def detectJacob(message):
+    temp = message.content.split('!')
+    print(temp)
+    nameID = temp[2].split('>')
+    if int(nameID[0]) == nickNameID[Jacob]:
+        return True
     else:
         return False
 
@@ -144,7 +183,13 @@ async def detectParamater(message):
 async def parseMessageParamaterName(message):
     firstPart = message.content.split('<') #this means index 1 will hold the value "mytext>"
     actualParam = firstPart[1].split('>')
-    return actualParam[0] 
+    return actualParam[0]
+
+#When you pass a param with a tag i.e., @Jacob it is actually an ID number. SO we need to get it.
+async def scanIDs(message):
+    firstPart = message.content.split('@')
+    idNum = firstPart[1].split('<')
+    return idNum[0]
 
 async def sys_error(num):
     if num == 1:
